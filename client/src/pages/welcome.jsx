eb47550963229d8741d63f5baa903ebb501ff556
subhu0028 <116936244+subhu0028@users.noreply.github.com>
2024-08-09 08:16:48 +0530

    Updated welcome page

--- client/src/pages/welcome.jsx
+++ client/src/pages/welcome.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from 'react';
+import { ConnectButton } from '@rainbow-me/rainbowkit';
 
 const Welcome = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="flex flex-col items-center justify-center h-screen bg-[#121212]">
+      <h1 className="text-5xl font-bold mb-8 text-white" style={{ fontFamily: 'ClashGrotesk' }}>
+        Welcome
+      </h1>
+      <div>
+        <ConnectButton.Custom>
+          {({ account, openAccountModal, openConnectModal, openChainModal, mounted }) => {
+            // Render nothing if the button is not mounted
+            if (!mounted) {
+              return null;
+            }
 
-export default Welcome
+            if (!account) {
+              return (
+                <button
+                  onClick={openConnectModal}
+                  className="bg-[#6b65c2] text-white py-3 px-6 rounded-lg text-lg hover:bg-[#6b65c2] transition-colors"
+                >
+                  Connect Wallet
+                </button>
+              );
+            } else {
+              return (
+                <button
+                  onClick={openAccountModal}
+                  className="bg-[#6b65c2] text-white py-3 px-6 rounded-lg text-lg hover:bg-[#5953a1] transition-colors"
+                >
+                  Wallet Connected
+                </button>
+              );
+            }
+          }}
+        </ConnectButton.Custom>
+      </div>
+    </div>
+  );
+};
+
+export default Welcome;
